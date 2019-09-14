fd987efd65921110c484b28882216636912bb314
haoyuegongzi <chen126jie@163.com>
2019-09-14 23:36:33 +0800

    更改登录的View层及布局

--- app/src/main/java/com/wanandroid/logic/login/view/ViewLoginInterface.java
+++ app/src/main/java/com/wanandroid/logic/login/view/ViewLoginInterface.java
@@ -6,7 +6,7 @@ import com.wanandroid.logic.login.moudle.UserInfo;
 /**
  * 作者：Created by Administrator on 2018/4/17.
  * 邮箱：chen126jie@163.com
- * TODO：
+ * TODO：loginActivity登录页的View层，不过现在已经有了更先进的写法了。
  */
 
 public interface ViewLoginInterface {
@@ -14,4 +14,5 @@ public interface ViewLoginInterface {
     UserInfo getUserInfo(String username, String password);
     void loginSuccess(int loginCode, LoginBean loginBean);
     void loginFailed(int lofinCode, LoginBean loginBean);
+
 }
